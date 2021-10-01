7d98ebd249a00d7cb8511391b9b41fd1bfe61a02
Dermofet <71003028+Dermofet@users.noreply.github.com>
2021-10-01 11:16:00 +0300

    Update test.cpp

--- lab2/test.cpp
+++ lab2/test.cpp
@@ -11,18 +11,46 @@ TEST(ALL_CONSTRUCTOR, CONSTRUCTOR_1)
     ASSERT_LT(C.get_Vert().y, C.get_RLim().y);
 }
 
-TEST(ALL_GETTERS, GETTER_1)
+TEST(ALL_FIND, FIND_Y)
 {
     geom::Catenary C = geom::Catenary(-5, 3, 0, 2);
 
-    ASSERT_FLOAT_EQ(3.7768478, C.get_y((float)2.5, true));
-    ASSERT_FLOAT_EQ(7.8966355, C.get_y((float)-4.1, true));
+    ASSERT_FLOAT_EQ(3.7768478, C.find_y((float)2.5, true));
+    ASSERT_FLOAT_EQ(7.8966355, C.find_y((float)-4.1, true));
+    ASSERT_ANY_THROW(C.find_y((float)-6, true));
+    ASSERT_ANY_THROW(C.find_y((float)4, true));
+}
 
-    ASSERT_FLOAT_EQ(16.358967, C.get_ArcLength());
-    ASSERT_FLOAT_EQ(3.35241, C.get_RadOfCurvature((float)-1.5));
-    ASSERT_FLOAT_EQ(5.638217, C.get_CenterOfCurvature((float)-1.5).x);
-    ASSERT_FLOAT_EQ(5.9417763, C.get_CenterOfCurvature((float)-1.5).y);
-    ASSERT_FLOAT_EQ(32.717934, C.get_Square());
+TEST(ALL_FIND, FIND_ARCLENGTH)
+{
+    geom::Catenary C = geom::Catenary(-5, 3, 0, 2);
+    ASSERT_FLOAT_EQ(16.358967, C.find_ArcLength());
+}
+
+TEST(ALL_FIND, FIND_RADOFCURVUATE)
+{
+    geom::Catenary C = geom::Catenary(-5, 3, 0, 2);
+
+    ASSERT_FLOAT_EQ(3.35241, C.find_RadOfCurvature((float)-1.5));
+    ASSERT_ANY_THROW(C.find_RadOfCurvature((float)-6));
+    ASSERT_ANY_THROW(C.find_RadOfCurvature((float)4));
+}
+
+TEST(ALL_FIND, FIND_CENTEROFCURVUATE)
+{
+    geom::Catenary C = geom::Catenary(-5, 3, 0, 2);
+
+    ASSERT_FLOAT_EQ(5.638217, C.find_CenterOfCurvature((float)-1.5).x);
+    ASSERT_FLOAT_EQ(5.9417763, C.find_CenterOfCurvature((float)-1.5).y);
+    ASSERT_ANY_THROW(C.find_CenterOfCurvature((float)-6));
+    ASSERT_ANY_THROW(C.find_CenterOfCurvature((float)4));
+}
+
+TEST(ALL_FIND, FIND_AREA)
+{
+    geom::Catenary C = geom::Catenary(-5, 3, 0, 2);
+
+    ASSERT_FLOAT_EQ(32.717934, C.find_Area());
 }
 
 int main(int argc, char** argv) {
